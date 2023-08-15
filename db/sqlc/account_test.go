32b0f372f5fde86b496a79ff6be1f395df24af3a
CrossStack-Q <crossstack@gmail.com>
2023-08-15 22:46:38 +0530

    15Aug

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -19,4 +19,11 @@ func TestCreateAccount(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, account)
 
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
 }
